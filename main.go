0dc6095bae76197dba416d3f5f0c4e0b4c8cc5d4
Maksim Donets <maks.donets.95@outlook.com>
2024-01-02 20:11:22 +0300

    add using middleware

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ func main() {
 
 func BuildServer() {
 	mux := mux.NewRouter()
-	mux.HandleFunc("/info", controllers.Info)
-	mux.HandleFunc("/search", controllers.Search)
-	mux.HandleFunc("/delete/by/id", controllers.DeleteById)
+	mux.HandleFunc("/info", controllers.MW(controllers.Info))
+	mux.HandleFunc("/search", controllers.MW(controllers.Search))
+	mux.HandleFunc("/delete/by/id", controllers.MW(controllers.DeleteById))
 	mux.HandleFunc("/create", controllers.Create)
-	mux.HandleFunc("/update/number", controllers.UpdateNumber)
-	mux.HandleFunc("/update/name", controllers.Update)
-	mux.HandleFunc("/update/list", controllers.Update)
+	mux.HandleFunc("/update/number", controllers.MW(controllers.UpdateNumber))
+	mux.HandleFunc("/update/name", controllers.MW(controllers.Update))
+	mux.HandleFunc("/update/list", controllers.MW(controllers.Update))
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
