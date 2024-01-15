44e4173abf8f42ba4015e2a044d14837112b6231
Maksim Donets <maks.donets.95@outlook.com>
2024-01-15 20:06:22 +0300

    small  fix

--- main.go
+++ main.go
@@ -8,12 +8,6 @@ import (
 	"github.com/udonetsm/cmngb/controllers"
 )
 
-type test struct {
-	Name   string
-	Number string
-	List   []int
-}
-
 func main() {
 	BuildServer()
 }
@@ -24,7 +18,7 @@ func BuildServer() {
 	mux.HandleFunc("/search", controllers.Search)
 	mux.HandleFunc("/delete/by/id", controllers.MW(controllers.DeleteById))
 	mux.HandleFunc("/create", controllers.MW(controllers.Create))
-	mux.HandleFunc("/update/number", controllers.MW(controllers.UpdateNumber))
+	mux.HandleFunc("/update/number", controllers.MW(controllers.Update))
 	mux.HandleFunc("/update/name", controllers.MW(controllers.Update))
 	mux.HandleFunc("/update/list", controllers.MW(controllers.Update))
 	server := &http.Server{
