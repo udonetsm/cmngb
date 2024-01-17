275ef9fbfcba1728d782e4912a633935aa0c9dfb
Maksim Donets <maks.donets.95@outlook.com>
2024-01-17 12:45:20 +0300

    delete separate update URIs

--- main.go
+++ main.go
@@ -16,11 +16,9 @@ func BuildServer() {
 	mux := mux.NewRouter()
 	mux.HandleFunc("/info", controllers.MW(controllers.Info))
 	mux.HandleFunc("/search", controllers.Search)
-	mux.HandleFunc("/delete/by/id", controllers.MW(controllers.DeleteById))
+	mux.HandleFunc("/delete", controllers.MW(controllers.DeleteById))
 	mux.HandleFunc("/create", controllers.MW(controllers.Create))
-	mux.HandleFunc("/update/number", controllers.MW(controllers.Update))
-	mux.HandleFunc("/update/name", controllers.MW(controllers.Update))
-	mux.HandleFunc("/update/list", controllers.MW(controllers.Update))
+	mux.HandleFunc("/update", controllers.MW(controllers.Update))
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
