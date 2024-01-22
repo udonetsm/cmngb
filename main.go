d6631d8e9c22a2ad06b3de6421b4ee4617333429
Maksim Donets <maks.donets.95@outlook.com>
2024-01-22 18:51:07 +0300

    rename old uris and addcreate user uri

--- main.go
+++ main.go
@@ -6,25 +6,21 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/udonetsm/cmngb/controllers"
-	"github.com/udonetsm/cmngb/flags"
 )
 
 func main() {
 	BuildServer()
 }
 
-func init() {
-	flags.Load()
-}
-
 func BuildServer() {
 	mux := mux.NewRouter()
-	mux.HandleFunc("/inf", controllers.MW(controllers.Info))
-	mux.HandleFunc("/lst", controllers.Search)
-	mux.HandleFunc("/del", controllers.MW(controllers.Delete))
-	mux.HandleFunc("/new", controllers.MW(controllers.Create))
-	mux.HandleFunc("/upd", controllers.MW(controllers.Update))
-	mux.HandleFunc("/auth", controllers.Auth)
+	mux.HandleFunc("/info", controllers.MW(controllers.Info))
+	mux.HandleFunc("/get/list", controllers.Search)
+	mux.HandleFunc("/delele", controllers.MW(controllers.Delete))
+	mux.HandleFunc("/create", controllers.MW(controllers.Create))
+	mux.HandleFunc("/update", controllers.MW(controllers.Update))
+	mux.HandleFunc("/get/token", controllers.Auth)
+	mux.HandleFunc("/create/user", controllers.NewUser)
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
