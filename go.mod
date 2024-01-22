d022cd2f456da690bbf702ec10e657b86242f2dc
Maksim Donets <maks.donets.95@outlook.com>
2024-01-22 12:23:00 +0300

    Add auth uri. This is a part of separating users.

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func BuildServer() {
 	mux.HandleFunc("/del", controllers.MW(controllers.Delete))
 	mux.HandleFunc("/new", controllers.MW(controllers.Create))
 	mux.HandleFunc("/upd", controllers.MW(controllers.Update))
+	mux.HandleFunc("/auth", controllers.Auth)
 	server := &http.Server{
 		Handler: mux,
 		Addr:    ":8080",
