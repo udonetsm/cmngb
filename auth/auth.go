e9aeb2d26051c532cb9f478238019e6ba85d595b
Maksim Donets <maks.donets.95@outlook.com>
2024-01-29 12:42:13 +0300

    remove adding secret in the token..

--- auth/auth.go
+++ auth/auth.go
@@ -10,12 +10,12 @@ func CreateToken(e *models.Entries, livetime int64) {
 		ExpiresAt: livetime,
 		Id:        e.Owner,
 	})
-	e.Token, e.Error = unsignedToken.SignedString([]byte(e.Secret + e.Owner))
+	e.Token, e.Error = unsignedToken.SignedString([]byte(e.Owner))
 }
 
 func TokenValid(e *models.Entries) bool {
 	token, err := jwt.ParseWithClaims(e.Token, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
-		return []byte(e.Secret + e.Owner), nil
+		return []byte(e.Owner), nil
 	})
 	e.Error = err
 	return token.Valid
